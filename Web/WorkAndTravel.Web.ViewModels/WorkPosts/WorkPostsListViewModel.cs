eefb32fa3346ac270180fa06925892422e8809b9
YordanSimov <yordansimov3@gmail.com>
2020-11-22 01:16:05 +0200

    Made partial view for the pagination and fixed bugs in its view

--- Web/WorkAndTravel.Web.ViewModels/WorkPosts/WorkPostsListViewModel.cs
+++ Web/WorkAndTravel.Web.ViewModels/WorkPosts/WorkPostsListViewModel.cs
@@ -3,24 +3,8 @@
     using System;
     using System.Collections.Generic;
 
-    public class WorkPostsListViewModel
+    public class WorkPostsListViewModel : PagingViewModel
     {
         public IEnumerable<WorkPostInListViewModel> WorkPosts { get; set; }
-
-        public int PageNumber { get; set; }
-
-        public int WorkPostsCount { get; set; }
-
-        public int ItemsPerPage { get; set; }
-
-        public bool HasPreviousPage => this.PageNumber > 1;
-
-        public bool HasNextPage => this.PageNumber < this.PagesCount;
-
-        public int PreviousPageNumber => this.PageNumber - 1;
-
-        public int NextPageNumber => this.PageNumber + 1;
-
-        public int PagesCount => (int)Math.Ceiling((double)this.WorkPostsCount / this.ItemsPerPage);
     }
 }
